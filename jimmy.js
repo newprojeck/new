b39e6ca3d384f2778d5f46db739ac46daa712be8
syamsul Nazri <syamsul Nazri>
2016-03-16 15:49:29 +0800

    initial commit

---
+++ jimmy.js
@@ -0,0 +1,7 @@
+var io = require('socket.io')(3600);
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
